d8b467fb3fa36d10e880decb453352668f2afab7
Sahil <sahilsinghzx77@gmail.com>
2022-03-30 01:28:38 +0530

    Basic Data Display (NW)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,7 +4,34 @@ import 'package:flutter/material.dart';
 import 'package:sportify/constants.dart';
 import 'package:sportify/gradient_text.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../helper/news.dart';
+import '../models/article_model.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<ArticleModel> articles = List<ArticleModel>();
+
+  bool _loading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getNews();
+  }
+
+  getNews() async {
+    News newsObj = News();
+    await newsObj.getNews();
+    articles = newsObj.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,19 +43,32 @@ class HomeScreen extends StatelessWidget {
               LinearGradient(colors: [Color(0xFF4568dc), Color(0xFFb06ab3)]),
         ),
       ),
+      body: _loading
+          ? Center(child: Container(child: CircularProgressIndicator()))
+          : Container(
+              child: ListView.builder(
+                  itemCount: articles.length,
+                  itemBuilder: (context, index) {
+                    return Tile(
+                      urlToImage: articles[index].urlToImage,
+                      title: articles[index].title,
+                      description: articles[index].description,
+                    );
+                  })),
     );
   }
 }
 
 class Tile extends StatelessWidget {
-  const Tile({key, this.imageUrl, this.title, this.desc}) : super(key: key);
-  final String imageUrl, title, desc;
+  const Tile({key, this.urlToImage, this.title, this.description})
+      : super(key: key);
+  final String urlToImage, title, description;
 
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Column(
-      children: [Image.network(imageUrl), Text(title), Text(desc)],
+      children: [Image.network(urlToImage), Text(title), Text(description)],
     ));
   }
 }
